c77805e4398cb8f911b02480115faac362731925
JulesBestDev176 <souleymanefall176@gmail.com>
2025-03-10 05:33:48 +0000

    jenkinsfile 6

--- projects/groupeisi/src/app/features/environments/environment.ts
+++ projects/groupeisi/src/app/features/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
   production: false,
   keycloak: {
     url: 'http://localhost:8180', // Utiliser le nom du service Docker
@@ -7,15 +6,3 @@ export const environment = {
     clientId: 'groupeisi'
   }
 };
-=======
-    production: false,
-    keycloak: {
-      url: 'http://localhost:8080',
-      realm: 'groupeisi',
-      clientId: 'groupeisi'
-    },
-    appName: 'Gestion établissement',
-    API_SERVER: 'http://localhost:9292',
-    APP_ROOT:'http://localhost:4200'
-  };
->>>>>>> 832332f58a4ecbd91d253ecaf7b0ecc55e892a73
